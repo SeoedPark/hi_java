a3aaf17246f2e7a50c0b19be0bb8122e5d5a2dc8
SeoedPark <field61@naver.com>
2023-08-07 12:24:15 +0900

    token

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -5,7 +5,7 @@ public class Hi_java {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("성공했네~~기쁘네");
-		
+		System.out.println("token유지바람");
 	} 
 	
 //*.java /ctrl + s/*.class/ctrl+f11/console
