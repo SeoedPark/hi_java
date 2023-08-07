4bf4694784e84d4fda57ccdf08bb5823a45a8078
SeoedPark <field61@naver.com>
2023-08-07 11:58:13 +0900

    수정

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -4,8 +4,13 @@ public class Hi_java {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
-	}
+		System.out.println("성공했네~~기쁘네");
+		
+	} 
+	
 //*.java /ctrl + s/*.class/ctrl+f11/console
+	
+	
 	
 }
 
